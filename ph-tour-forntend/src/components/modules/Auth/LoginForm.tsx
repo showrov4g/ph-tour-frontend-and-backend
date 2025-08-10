8ebd8f639a9a06af25baca8c76639db28433faae
showrov4g <showrovg00@gmail.com>
2025-08-10 22:41:35 +0600

    add form

--- ph-tour-forntend/src/components/modules/Auth/LoginForm.tsx
+++ ph-tour-forntend/src/components/modules/Auth/LoginForm.tsx
@@ -1,40 +1,33 @@
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router";
-
+import { GalleryVerticalEnd } from "lucide-react";
+import bgImage from "../../../assets/iamges/bg.jpg"
 const LoginForm = () => {
-  const form = useForm({
-    defaultValues: {
-      username: "",
-    },
-  });
+  
 
   return (
-    <Form {...form}>
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>If you are not register please <Link to={"/register"} className="text-red-600">Register</Link> </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </Form>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm/>
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src={bgImage}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
   );
 };
 
--- ph-tour-forntend/src/components/modules/Auth/RegisterForm.tsx
+++ ph-tour-forntend/src/components/modules/Auth/RegisterForm.tsx
@@ -1,40 +1,62 @@
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { GalleryVerticalEnd } from "lucide-react";
+import bgImage from "../../../assets/iamges/bg.jpg";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const RegisterForm = () => {
-  const form = useForm({
-    defaultValues: {
-      username: "",
-    },
-  });
+  const form = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <Form {...form}>
-      <FormField
-        control={form.control}
-        name="username"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
-            </FormControl>
-            <FormDescription>If you have already register please <Link to={"/login"} className="text-red-700">Login</Link></FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </Form>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src={bgImage}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                {/* =============== */}
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* =============== */}
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
